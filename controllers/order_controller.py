b657d38ab7189dde2710bf9636c29c7e0211343c
LidzDev <lydia.cordell@gmail.com>
2023-09-05 15:57:30 +0100

    order list displays

--- controllers/order_controller.py
+++ controllers/order_controller.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
 from flask import render_template
-#from models.task_list import tasks
+from models.order_list import orders
 
 orders_blueprint = Blueprint("orders", __name__)
 
 @orders_blueprint.route("/orders")
 
 def index():    
-    return render_template('index.jinja') #, title="My Task List", tasks = tasks)
+    return render_template('index.jinja', title="Shop Order List", orders = orders)
