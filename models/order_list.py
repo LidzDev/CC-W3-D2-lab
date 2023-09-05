a036a1f19ec4d7bc95cc0e577b090df59d46ae3a
LidzDev <lydia.cordell@gmail.com>
2023-09-05 15:40:19 +0100

    prefab order data added

---
+++ models/order_list.py
@@ -0,0 +1,9 @@
+from models.order import Order
+
+order1 = Order("Becky", "03/02/2023", 1, "Knitting for dummies", "Bertha Koeman", "fair")
+order2 = Order("Mark", "14/02/2023", 1, "Pole Heroes", "Gwen Stefani", "good")
+order3 = Order("Mike", "04/05/2023", 1, "How to git", "Linus Torvald", "excellent")
+order4 = Order("John", "08/07/2023", 11, "Python for beginners", "Guido Guide", "new")
+order5 = Order("Jack", "28/08/2023", 1, "Unix Power Tools", "many", "fair")
+
+orders = [order1, order2, order3, order4, order5]
