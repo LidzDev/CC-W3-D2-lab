f4467c1c9ca03b35eb4a73adbe84f0d9c497d94c
LidzDev <lydia.cordell@gmail.com>
2023-09-05 16:25:41 +0100

    basic indiv order jinja added

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from controllers.order_controller import orders_blueprint
+from controllers.order_controller import indiv_order_blueprint
+
 
 
 app = Flask(__name__)
 app.register_blueprint(orders_blueprint)
+app.register_blueprint(indiv_order_blueprint)
--- controllers/order_controller.py
+++ controllers/order_controller.py
@@ -3,8 +3,12 @@ from flask import render_template
 from models.order_list import orders
 
 orders_blueprint = Blueprint("orders", __name__)
+indiv_order_blueprint = Blueprint("indiv", __name__)
 
 @orders_blueprint.route("/orders")
-
 def index():    
     return render_template('index.jinja', title="Shop Order List", orders = orders)
+
+@indiv_order_blueprint.route("/indiv")
+def indiv():
+    return render_template('indiv.jinja')
