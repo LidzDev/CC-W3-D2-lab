8eb980085c352a072be22467cee86bce3362438e
LidzDev <lydia.cordell@gmail.com>
2023-09-05 20:53:03 +0100

    the individual order knows its number

--- controllers/order_controller.py
+++ controllers/order_controller.py
@@ -3,12 +3,12 @@ from flask import render_template
 from models.order_list import orders
 
 orders_blueprint = Blueprint("orders", __name__)
-indiv_order_blueprint = Blueprint("indiv", __name__)
 
 @orders_blueprint.route("/orders")
 def index():    
     return render_template('index.jinja', title="Shop Order List", orders = orders)
 
-@indiv_order_blueprint.route("/orders/")
-def indiv():
-    return render_template('indiv.jinja')
+@orders_blueprint.route("/orders/<index>")
+def indiv(index):
+    order_number = int(index)
+    return render_template('indiv.jinja', title="Order", order = orders[order_number], order_number = order_number)
